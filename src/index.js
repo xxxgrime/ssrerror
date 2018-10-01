be5cd5f82373e2930e68007c46ebf237bc0a97a9
xxxgrime <mxer12390@gmail.com>
2018-10-01 21:13:32 +0530

    fixed the start problem

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+var Timepass=(props)=>{
+  return (
+    <p>Welcome to react</p>
+  )
+}
+
+  ReactDOM.render(<Timepass/>,document.getElementById("index"));
